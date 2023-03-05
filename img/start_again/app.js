68bd2658a964b0b659b6b13698b2cc2584c98eda
Nayeem <itsnayeem03@gmail.com>
2023-03-05 23:01:50 +0600

    upload ruki files

---
+++ img/start_again/app.js
@@ -0,0 +1,9 @@
+console.log('hello world');
+let name = 'nayeem';
+let age = 17;
+
+if(age <=0 && age <= 21){
+    console.log('you can marrige.');
+}else{
+    console.log(`${name} you can not marrige at this age you need more ${21 -age}`);
+}
